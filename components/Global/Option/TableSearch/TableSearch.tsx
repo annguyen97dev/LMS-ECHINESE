ead69771e0721ad30d3a2dbf0405c77d9da73903
annguyen97dev <annguyen97dev@gmail.com>
2021-07-12 23:50:35 +0700

    up

--- components/Global/Option/TableSearch/TableSearch.tsx
+++ components/Global/Option/TableSearch/TableSearch.tsx
@@ -1,116 +1,116 @@
-import {SearchOutlined} from '@ant-design/icons';
-import {Button, DatePicker, Input, Space} from 'antd';
-import moment from 'moment';
-import React, {useEffect, useRef, useState} from 'react';
+import { SearchOutlined } from "@ant-design/icons";
+import { Button, DatePicker, Input, Space } from "antd";
+import moment from "moment";
+import React, { useEffect, useRef, useState } from "react";
 
-const TableSearch = (dataIndex, handleSearch, handleReset, type = 'text') => {
-	const [isVisible, setIsVisible] = useState(false);
-	const [valueSearch, setValueSearch] = useState<any>(null);
-	const {RangePicker} = DatePicker;
-	const inputRef = useRef<any>(null);
+const TableSearch = (dataIndex, handleSearch, handleReset, type = "text") => {
+  const [isVisible, setIsVisible] = useState(false);
+  const [valueSearch, setValueSearch] = useState<any>(null);
+  const { RangePicker } = DatePicker;
+  const inputRef = useRef<any>(null);
 
-	const getValueSearch = (value) => {
-		setValueSearch(value);
-	};
+  const getValueSearch = (value) => {
+    setValueSearch(value);
+  };
 
-	const checkHandleSearch = (value) => {
-		if (!handleSearch) return;
-		if (!valueSearch) return;
-		handleSearch(value);
-		getValueSearch(null);
-		setIsVisible(false);
-	};
+  const checkHandleSearch = (value) => {
+    if (!handleSearch) return;
+    if (!valueSearch) return;
+    handleSearch(value);
+    getValueSearch(null);
+    setIsVisible(false);
+  };
 
-	const checkHandleReset = () => {
-		if (!handleReset) return;
-		handleReset();
-		getValueSearch(null);
-		setIsVisible(false);
-	};
+  const checkHandleReset = () => {
+    if (!handleReset) return;
+    handleReset();
+    getValueSearch(null);
+    setIsVisible(false);
+  };
 
-	const checkType = () => {
-		let fControl;
-		switch (type) {
-			case 'text':
-				fControl = (
-					<Input
-						ref={inputRef}
-						value={valueSearch}
-						placeholder={`Search ${dataIndex}`}
-						onPressEnter={(e) => checkHandleSearch(valueSearch)}
-						onChange={(e) => getValueSearch(e.target.value)}
-						style={{marginBottom: 8, display: 'block'}}
-					/>
-				);
-				break;
-			case 'date':
-				fControl = (
-					<DatePicker
-						style={{marginBottom: 8, display: 'block'}}
-						autoFocus={true}
-						format="DD/MM/YYYY"
-						onChange={(date, dateString) => getValueSearch(date)}
-					/>
-				);
-				break;
-			case 'date-range':
-				fControl = (
-					<div style={{marginBottom: 8, display: 'block'}}>
-						<RangePicker
-							format="DD/MM/YYYY"
-							autoFocus={true}
-							ranges={{
-								Today: [moment(), moment()],
-								'This Month': [
-									moment().startOf('month'),
-									moment().endOf('month'),
-								],
-							}}
-							onChange={(date, dateString) => getValueSearch(date)}
-						/>
-					</div>
-				);
-				break;
-			default:
-				break;
-		}
-		return fControl;
-	};
-	useEffect(() => {
-		if (isVisible) {
-			setTimeout(() => {
-				inputRef.current?.select?.();
-			}, 100);
-		}
-	}, [isVisible]);
-	const getColumnSearchProps = (dataIndex) => ({
-		filterDropdown: () => (
-			<div style={{padding: 8}}>
-				{checkType()}
-				<Space>
-					<Button
-						type="primary"
-						onClick={() => checkHandleSearch(valueSearch)}
-						icon={<SearchOutlined />}
-						size="small"
-						style={{width: 90}}
-					>
-						Search
-					</Button>
-					<Button onClick={checkHandleReset} size="small" style={{width: 90}}>
-						Reset
-					</Button>
-				</Space>
-			</div>
-		),
-		filterDropdownVisible: isVisible,
-		filterIcon: (filtered) => <SearchOutlined />,
-		onFilterDropdownVisibleChange: (visible) => {
-			visible ? setIsVisible(true) : setIsVisible(false);
-		},
-	});
+  const checkType = () => {
+    let fControl;
+    switch (type) {
+      case "text":
+        fControl = (
+          <Input
+            ref={inputRef}
+            value={valueSearch}
+            placeholder={`Search ${dataIndex}`}
+            onPressEnter={(e) => checkHandleSearch(valueSearch)}
+            onChange={(e) => getValueSearch(e.target.value)}
+            style={{ marginBottom: 8, display: "block" }}
+          />
+        );
+        break;
+      case "date":
+        fControl = (
+          <DatePicker
+            style={{ marginBottom: 8, display: "block" }}
+            autoFocus={true}
+            format="DD/MM/YYYY"
+            onChange={(date, dateString) => getValueSearch(date)}
+          />
+        );
+        break;
+      case "date-range":
+        fControl = (
+          <div style={{ marginBottom: 8, display: "block" }}>
+            <RangePicker
+              format="DD/MM/YYYY"
+              autoFocus={true}
+              ranges={{
+                Today: [moment(), moment()],
+                "This Month": [
+                  moment().startOf("month"),
+                  moment().endOf("month"),
+                ],
+              }}
+              onChange={(date, dateString) => getValueSearch(date)}
+            />
+          </div>
+        );
+        break;
+      default:
+        break;
+    }
+    return fControl;
+  };
+  useEffect(() => {
+    if (isVisible) {
+      setTimeout(() => {
+        inputRef.current?.select?.();
+      }, 100);
+    }
+  }, [isVisible]);
+  const getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: () => (
+      <div style={{ padding: 8 }}>
+        {checkType()}
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => checkHandleSearch(valueSearch)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Search
+          </Button>
+          <Button onClick={checkHandleReset} size="small" style={{ width: 90 }}>
+            Reset
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterDropdownVisible: isVisible,
+    filterIcon: (filtered) => <SearchOutlined />,
+    onFilterDropdownVisibleChange: (visible) => {
+      visible ? setIsVisible(true) : setIsVisible(false);
+    },
+  });
 
-	return getColumnSearchProps(dataIndex);
+  return getColumnSearchProps(dataIndex);
 };
 
 export default TableSearch;
--- pages/option/provincial/index.tsx
+++ pages/option/provincial/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import Area from '~/components/Global/Option/Area/Area';
-import LayoutBase from '~/components/LayoutBase';
+import React from "react";
+import Area from "~/components/Global/Option/Area/Area";
+import LayoutBase from "~/components/LayoutBase";
 
 const AreaPage = () => <Area />;
 
