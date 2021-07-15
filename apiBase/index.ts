602f1cc7e96a37d7b91d03a1fed7e1591df93e95
annguyen97dev <annguyen97dev@gmail.com>
2021-07-15 09:21:42 +0700

    update final

--- apiBase/index.ts
+++ apiBase/index.ts
@@ -8,28 +8,8 @@ export { studyTimeApi } from "./options/study-time";
 export { curriculumApi } from "./options/curriculum";
 export { serviceApi } from "./options/service";
 export { staffSalaryApi } from "./options/staff-salary";
-<<<<<<< HEAD
 export { jobApi } from "./options/job";
-// export const addDataDistrict = async (data) => {
-//   let result;
-//   try {
-//     console.log("Data in instance: ", data);
 
-//     const formdata = new FormData();
-//     formdata.append("ListCourseCode", data.ListCourseCode);
-//     formdata.append("ListCourseName", data.ListCourseName);
-//     formdata.append("Description", data.Description);
-//     // formdata.append("Enable", "true");
-//     let res = await instance.post("/api/Course/InsertCourse", formdata, {});
-//     result = res.data;
-//   } catch (error) {
-//     console.log("Error: ", error);
-//     return error.message ? error.message : (result = "");
-//   }
-//   return result;
-// };
-=======
 export { userInformationApi } from "./options/userinformation";
 export { puroseApi } from "./options/purose";
 export { supplierApi } from "./options/supplier";
->>>>>>> tufe
