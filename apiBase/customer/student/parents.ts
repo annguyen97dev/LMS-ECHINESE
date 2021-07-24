d7124209c12df2ba943a25caab0d3160d77275d6
linhju7y <linh.mhl9@gmail.com>
2021-07-24 18:29:10 +0700

    api parents

---
+++ apiBase/customer/student/parents.ts
@@ -0,0 +1,19 @@
+import { instance } from "~/apiBase/instance";
+
+const url = "/api/Parents";
+
+class ParentsApi {
+  getAll = (Params: any) =>
+    instance.get<IApiResultData<IParents[]>>(url, {
+      params: Params,
+    });
+
+  getDetail = (id: number) =>
+    instance.get<IApiResultData<IParents>>(`${url}/${id}`);
+
+  add = (data: IParents) => instance.post(url, data);
+
+  update = (data: IParents) => instance.put(url, data, {});
+}
+
+export const parentsApi = new ParentsApi();
---
+++ apiBase/types/customer/student/parents.d.ts
@@ -0,0 +1,27 @@
+type IParents = IBaseApi<{
+  UserInformationID: number;
+  UserAccountID: number;
+  UserName: string;
+  Password: string;
+  FullNameUnicode: string;
+  Address: string;
+  AreaID: number;
+  AreaName: string;
+  DistrictID: number;
+  DistrictName: string;
+  WardID: number;
+  WardName: string;
+  HouseNumber: string;
+  Avatar: string;
+  Branch: string;
+  CMND: string;
+  CMNDDate: string;
+  CMNDRegister: string;
+  DOB: string;
+  Email: string;
+  Extension: string;
+  Gender: number;
+  StatusID: number;
+  StatusName: string;
+  Mobile: string;
+}>;
