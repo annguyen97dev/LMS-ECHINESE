f0795166bea2bc13a443fc5d1b644ed914052653
linhju7y <linh.mhl9@gmail.com>
2021-08-01 23:34:41 +0700

    api student course reserve

---
+++ apiBase/customer/student/course-reserve.ts
@@ -0,0 +1,23 @@
+import { instance } from "~/apiBase/instance";
+
+const url = "/api/CourseReserve";
+
+class CourseReserveApi {
+  getAll = (Params: any) =>
+    instance.get<IApiResultData<ICourseReserve[]>>(url, {
+      params: Params,
+    });
+
+  getDetail = (id: number) =>
+    instance.get<IApiResultData<ICourseReserve>>(`${url}/${id}`);
+
+  add = (data: ICourseReserve) => instance.post(url, data);
+
+  update = (data: ICourseReserve) => instance.put(url, data, {});
+
+  // reserve insert student to course
+  reserveAddCourse = (data: ICourseReserve) =>
+    instance.post("/api/ReserveInsertCourse", data);
+}
+
+export const courseReserveApi = new CourseReserveApi();
