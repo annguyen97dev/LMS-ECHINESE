a44afb9d5132fdea31654258bd8241d793adea4f
BaoFE98 <kimbao756@yahoo.com>
2021-08-10 10:54:21 +0700

    CourseList: student list

---
+++ apiBase/course-detail/student-list.ts
@@ -0,0 +1,19 @@
+import {instance} from '../instance';
+
+const url = '/api/StudentInCourse/';
+type IApiResultStudentInCourse<T = any> = {
+	studentList: T;
+	message: string;
+	TotalRow: number;
+};
+export const studentListInCourseApi = {
+	// Lấy tất cả data
+	getAll(params) {
+		return instance.get<IApiResultStudentInCourse<IStudentListInCourse[]>>(
+			url,
+			{
+				params,
+			}
+		);
+	},
+};
