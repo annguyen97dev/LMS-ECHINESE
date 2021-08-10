278bf849848f2a8e40781b5fbfdaa65c67fd8669
annguyen97dev <annguyen97dev@gmail.com>
2021-08-10 09:28:56 +0700

    update anfe

--- components/Elements/Editor/Editor.tsx
+++ components/Elements/Editor/Editor.tsx
@@ -1,50 +1,90 @@
-import React from "react";
-import ReactDOM from "react-dom";
+// import React from "react";
+// import ReactDOM from "react-dom";
 
-import { CKEditor } from "@ckeditor/ckeditor5-react";
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
-import Base64UploadAdapter from "@ckeditor/ckeditor5-upload/src/adapters/base64uploadadapter";
+// import { CKEditor } from "@ckeditor/ckeditor5-react";
+// import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+// import Base64UploadAdapter from "@ckeditor/ckeditor5-upload/src/adapters/base64uploadadapter";
 
-// import "./styles.css";
+// // import "./styles.css";
 
-// https://ckeditor.com/docs/ckeditor5/latest/builds/guides/integration/frameworks/react.html
+// // https://ckeditor.com/docs/ckeditor5/latest/builds/guides/integration/frameworks/react.html
 
-/* import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials";
-import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold";
-import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic";
-import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph"; */
+// /* import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials";
+// import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold";
+// import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic";
+// import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph"; */
 
-/* const editorConfiguration = {
-  plugins: [Essentials, Bold, Italic, Paragraph],
-  toolbar: ["bold", "italic"]
-}; */
+// /* const editorConfiguration = {
+//   plugins: [Essentials, Bold, Italic, Paragraph],
+//   toolbar: ["bold", "italic"]
+// }; */
 
-function Editor(props) {
-  const { getDataEditor } = props;
+// function Editor(props) {
+//   const { getDataEditor } = props;
+
+//   return (
+//     <div className="App">
+//       <CKEditor
+//         editor={ClassicEditor}
+//         data=""
+//         onReady={(editor) => {
+//           // You can store the "editor" and use when it is needed.
+//           console.log("Editor is ready to use!", editor);
+//         }}
+//         onChange={(event, editor) => {
+//           const data = editor.getData();
+//           getDataEditor(data);
+//           console.log({ event, editor, data });
+//         }}
+//         onBlur={(event, editor) => {
+//           console.log("Blur.", editor);
+//         }}
+//         onFocus={(event, editor) => {
+//           console.log("Focus.", editor);
+//         }}
+//       />
+//     </div>
+//   );
+// }
+
+// export default Editor;
+
+import React, { Component } from "react";
+import ReactSummernote from "react-summernote";
+import "react-summernote/dist/react-summernote.css"; // import styles
+import "react-summernote/lang/summernote-ru-RU"; // you can import any other locale
+
+// Import bootstrap(v3 or v4) dependencies
+import "bootstrap/js/src/modal";
+import "bootstrap/js/src/dropdown";
+import "bootstrap/js/src/tooltip";
+import "bootstrap/dist/css/bootstrap.css";
+
+const EditorSummernote = () => {
+  const onChange = (content) => {
+    console.log("onChange", content);
+  };
 
   return (
-    <div className="App">
-      <CKEditor
-        editor={ClassicEditor}
-        data=""
-        onReady={(editor) => {
-          // You can store the "editor" and use when it is needed.
-          console.log("Editor is ready to use!", editor);
-        }}
-        onChange={(event, editor) => {
-          const data = editor.getData();
-          getDataEditor(data);
-          console.log({ event, editor, data });
-        }}
-        onBlur={(event, editor) => {
-          console.log("Blur.", editor);
-        }}
-        onFocus={(event, editor) => {
-          console.log("Focus.", editor);
-        }}
-      />
-    </div>
+    <ReactSummernote
+      value="Default value"
+      options={{
+        lang: "vn",
+        height: 350,
+        dialogsInBody: true,
+        toolbar: [
+          ["style", ["style"]],
+          ["font", ["bold", "underline", "clear"]],
+          ["fontname", ["fontname"]],
+          ["para", ["ul", "ol", "paragraph"]],
+          ["table", ["table"]],
+          ["insert", ["link", "picture", "video"]],
+          ["view", ["fullscreen", "codeview"]],
+        ],
+      }}
+      onChange={onChange}
+    />
   );
-}
+};
 
-export default Editor;
+export default EditorSummernote;
--- components/Elements/Editor/index.tsx
+++ components/Elements/Editor/index.tsx
@@ -11,10 +11,50 @@ const Editor = (props) => {
   return (
     <div>
       <DynamicComponentWithNoSSR
-        getDataEditor={(value) => handleChange(value)}
+      // getDataEditor={(value) => handleChange(value)}
       />
     </div>
   );
 };
 
 export default Editor;
+
+// import React, { Component } from "react";
+// import ReactSummernote from "react-summernote";
+// import "react-summernote/dist/react-summernote.css"; // import styles
+// import "react-summernote/lang/summernote-ru-RU"; // you can import any other locale
+
+// // Import bootstrap(v3 or v4) dependencies
+// import "bootstrap/js/dist/modal";
+// import "bootstrap/js/dist/dropdown";
+// import "bootstrap/js/dist/tooltip";
+// import "bootstrap/dist/css/bootstrap.css";
+
+// const Editor = () => {
+//   const onChange = (content) => {
+//     console.log("onChange", content);
+//   };
+
+//   return (
+//     <ReactSummernote
+//       value="Default value"
+//       options={{
+//         lang: "ru-RU",
+//         height: 350,
+//         dialogsInBody: true,
+//         toolbar: [
+//           ["style", ["style"]],
+//           ["font", ["bold", "underline", "clear"]],
+//           ["fontname", ["fontname"]],
+//           ["para", ["ul", "ol", "paragraph"]],
+//           ["table", ["table"]],
+//           ["insert", ["link", "picture", "video"]],
+//           ["view", ["fullscreen", "codeview"]],
+//         ],
+//       }}
+//       onChange={onChange}
+//     />
+//   );
+// };
+
+// export default Editor;
