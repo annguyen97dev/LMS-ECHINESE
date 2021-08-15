6bdbdc721c8f4e07fbf5ad2b07d516d1e8be79fa
linhju7y <linh.mhl9@gmail.com>
2021-08-15 19:54:55 +0700

    update document

--- components/Global/CourseList/CourseListDetail/DocumentCourse.tsx
+++ components/Global/CourseList/CourseListDetail/DocumentCourse.tsx
@@ -1,82 +1,63 @@
 import { Card, Input, Select, Tooltip } from "antd";
-import Checkbox from "antd/lib/checkbox/Checkbox";
 import Link from "next/link";
-import React, { Fragment, useState } from "react";
-import { Eye } from "react-feather";
+import React, { Fragment, useEffect, useState } from "react";
 import TitlePage from "~/components/Elements/TitlePage";
-import PowerTable from "~/components/PowerTable";
-import { dataStudent } from "~/lib/customer/dataStudent";
-import { fileItems } from "~/lib/document-list/data";
-import FileManager, {
-  Permissions,
-  ItemView,
-} from "devextreme-react/file-manager";
-
-// export const AntdFileManager = <T extends AntdFileManagerNode>(
-//   props: AntdFileManagerProps<T>
-// ): ReactElement => {
-//   const { renderType = "table" } = props;
-//   const RenderType = getAntdRendererByName<T>(renderType);
-//   return <FileManager {...props} renderer={RenderType} />;
-// };
+import { Tree } from "antd";
+import { documentCategoryApi } from "~/apiBase/course-detail/document-category";
+import { useWrap } from "~/context/wrap";
 
 const DocumentCourse = (props) => {
-  const [itemViewMode, setItemViewMode] = useState("thumbnails");
+  const [isLoading, setIsLoading] = useState(false);
+  const [categoryDoc, setCategoryDoc] = useState<ICategoryDoc[]>([]);
+  const { showNoti } = useWrap();
 
-  const onOptionChanged = (e) => {
-    if (e.fullName === "itemView.mode") {
-      setItemViewMode(e.value);
-    }
+  const getDataCategoryDoc = () => {
+    setIsLoading(true);
+    (async () => {
+      try {
+        let res = await documentCategoryApi.getAll({
+          CourseID: 1,
+        });
+        //@ts-ignore
+        res.status == 200 && setCategoryDoc(res.data.data);
+        if (res.status == 204) {
+          showNoti("danger", "Không tìm thấy dữ liệu!");
+        }
+      } catch (error) {
+        showNoti("danger", error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    })();
   };
 
-  // const getAntdRendererByName = <T extends AntdFileManagerNode>(
-  //   name: "table"
-  // ): FileManagerRenderComponent<T> => {
-  //   if (name === "table") {
-  //     return AntdTableRenderer;
-  //   }
-  //   throw TypeError(`Unknown renderer ${name}`);
-  // };
+  useEffect(() => {
+    getDataCategoryDoc();
+    console.log(categoryDoc);
+  }, []);
 
-  const customizeIcon = (fileSystemItem) => {
-    if (fileSystemItem.isDirectory) {
-      return "https://js.devexpress.com/Demos/WidgetsGallery/JSDemos/images/thumbnails/folder.svg";
-    }
+  const { DirectoryTree } = Tree;
 
-    const fileExtension = fileSystemItem.getFileExtension();
-    switch (fileExtension) {
-      case ".txt":
-        return "https://js.devexpress.com/Demos/WidgetsGallery/JSDemos/images/thumbnails/doc-txt.svg";
-      case ".rtf":
-        return "https://js.devexpress.com/Demos/WidgetsGallery/JSDemos/images/thumbnails/doc-rtf.svg";
-      case ".xml":
-        return "https://js.devexpress.com/Demos/WidgetsGallery/JSDemos/images/thumbnails/doc-xml.svg";
-      case ".pdf":
-        return "/icons/231-pdf.svg";
-    }
-  };
+  const treeData = [
+    {
+      title: "parent 0",
+      key: "0-0",
+    },
+    {
+      title: "parent 1",
+      key: "0-1",
+    },
+  ];
 
   return (
     <>
       <Card title="Document course">
-        Document course
-        {/* <FileManager
-          fileSystemProvider={props.fileItems}
-          customizeThumbnail={customizeIcon}
-          height={750}
-          onOptionChanged={onOptionChanged}
-        >
-          <ItemView mode={itemViewMode}></ItemView>
-          <Permissions
-            create={true}
-            copy={true}
-            move={true}
-            delete={true}
-            rename={true}
-            upload={true}
-            download={true}
-          ></Permissions>
-        </FileManager> */}
+        <div className="row">
+          <div className="col-2">
+            <DirectoryTree treeData={treeData} />
+          </div>
+          <div className="col-10">file div</div>
+        </div>
       </Card>
     </>
   );
