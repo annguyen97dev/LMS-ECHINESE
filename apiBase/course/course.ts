5ee8aea270434fdfee2219ac29a9571303727aee
BaoFE98 <kimbao756@yahoo.com>
2021-09-06 18:13:43 +0700

    FIX: add more field typescript for ICourse

--- apiBase/course/course.ts
+++ apiBase/course/course.ts
@@ -1,23 +1,23 @@
-import { instance } from "../instance";
+import {instance} from '../instance';
 
-const url = "/api/Course/";
+const url = '/api/Course/';
 export const courseApi = {
-  // Lấy tất cả data
-  getAll(params) {
-    return instance.get<IApiResultData<ICourse[]>>(url, {
-      params,
-    });
-  },
-  // Thêm mới data
-  add(data) {
-    return instance.post(url, data);
-  },
-  // update mới data
-  update(data) {
-    return instance.put(url, data);
-  },
+	// Lấy tất cả data
+	getAll(params) {
+		return instance.get<IApiResultData<ICourse[]>>(url, {
+			params,
+		});
+	},
+	// Thêm mới data
+	add(data) {
+		return instance.post(url, data);
+	},
+	// update mới data
+	update(data) {
+		return instance.put(url, data);
+	},
 
-  getById(id: number) {
-    return instance.get<IApiResultData<ICourse[]>>(`${url}${id}`);
-  },
+	getById(id: number) {
+		return instance.get<IApiResultData<ICourseDetail>>(`${url}${id}`);
+	},
 };
