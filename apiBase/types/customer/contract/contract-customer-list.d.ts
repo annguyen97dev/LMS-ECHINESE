0f7432d091dee8fcac8adbf3b4300dd0fd6298f3
BaoFE98 <kimbao756@yahoo.com>
2021-09-06 23:38:54 +0700

    FIX: fix contract feature

--- apiBase/types/customer/contract/contract-customer-list.d.ts
+++ apiBase/types/customer/contract/contract-customer-list.d.ts
@@ -1,15 +1,10 @@
 type IContractCustomerList = IBaseApi<{
-    ID: number,
-    UserInformationID: number,
-    FullNameUnicode: string,
-    CourseID: number,
-    CourseName: string,
-    CourseOfStudentID: number,
-    ContractContent: any,
-    Enable: boolean,
-    CreatedOn: string,
-    CreatedBy: string,
-    ModifiedOn: string,
-    ModifiedBy: string
-  }>;
-  
+	ID: number;
+	UserInformationID: number;
+	FullNameUnicode: string;
+	CourseID: number;
+	CourseName: string;
+	CourseOfStudentID: number;
+	ContractContent: string;
+	Accept: boolean;
+}>;
