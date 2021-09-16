b3ee309ddc7de81e7b8ab20b6be0f6877516d2ba
BaoFE98 <kimbao756@yahoo.com>
2021-09-16 16:32:32 +0700

    UPDATE: StudentList View translate

--- components/Global/CourseList/CourseListDetail/StudentList/StudentList.tsx
+++ components/Global/CourseList/CourseListDetail/StudentList/StudentList.tsx
@@ -1,15 +1,21 @@
 import {Tooltip} from 'antd';
 import Link from 'next/link';
-import {useRouter} from 'next/router';
+import PropTypes from 'prop-types';
 import React, {useEffect, useState} from 'react';
 import {Eye} from 'react-feather';
 import {studentListInCourseApi} from '~/apiBase';
 import PowerTable from '~/components/PowerTable';
 import {useWrap} from '~/context/wrap';
 
-const StudentsList = () => {
-	const router = useRouter();
-	const {slug: ID} = router.query;
+StudentsList.propTypes = {
+	courseID: PropTypes.number,
+};
+StudentsList.defaultProps = {
+	courseID: 0,
+};
+
+function StudentsList(props) {
+	const {courseID: ID} = props;
 	const {showNoti} = useWrap();
 	const [studentList, setStudentList] = useState<IStudentListInCourse[]>([]);
 	const [isLoading, setIsLoading] = useState({
@@ -30,13 +36,13 @@ const StudentsList = () => {
 		});
 	};
 	const columns = [
-		{title: 'Student', dataIndex: 'StudentName'},
-		{title: 'Phone', dataIndex: 'Mobile'},
+		{title: 'Tên học viên', dataIndex: 'StudentName'},
+		{title: 'SĐT', dataIndex: 'Mobile'},
 		{title: 'Email', dataIndex: 'Email'},
-		{title: 'Academic', dataIndex: 'AcademicName'},
-		{title: 'Day off', dataIndex: 'DayOff'},
+		{title: 'Người quản lý', dataIndex: 'AcademicName'},
+		{title: 'Số ngày nghỉ', dataIndex: 'DayOff'},
 		{
-			title: 'Warning',
+			title: 'Cảnh báo',
 			dataIndex: 'Warning',
 			render: (bool) => bool && <span className="tag yellow">Warning</span>,
 		},
@@ -95,9 +101,9 @@ const StudentsList = () => {
 				dataSource={studentList}
 				getPagination={getPagination}
 				columns={columns}
-				Extra={<h5>Học viên</h5>}
+				Extra={<h5>Danh sách học viên</h5>}
 			/>
 		</>
 	);
-};
+}
 export default StudentsList;
