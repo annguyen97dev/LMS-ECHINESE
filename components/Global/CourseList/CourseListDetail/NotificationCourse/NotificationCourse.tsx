8b429f2a935802b96a7453551f2be7d3f08302fa
BaoFE98 <kimbao756@yahoo.com>
2021-09-20 23:54:22 +0700

    UPDATE: NotificationCourse in detail course

--- components/Global/CourseList/CourseListDetail/NotificationCourse/NotificationCourse.tsx
+++ components/Global/CourseList/CourseListDetail/NotificationCourse/NotificationCourse.tsx
@@ -1,115 +1,132 @@
-import moment from "moment";
-import React, { useEffect, useState } from "react";
-import { notificationCourseApi } from "~/apiBase/course-detail/notification-course";
-import LayoutBase from "~/components/LayoutBase";
-import PowerTable from "~/components/PowerTable";
-import { useWrap } from "~/context/wrap";
-import NotificationCreate from "./NotificationCreate";
+import moment from 'moment';
+import PropTypes from 'prop-types';
+import React, {useEffect, useState} from 'react';
+import {notificationCourseApi} from '~/apiBase/course-detail/notification-course';
+import PowerTable from '~/components/PowerTable';
+import {useWrap} from '~/context/wrap';
+import NotificationCourseForm from './NotificationCourseForm';
 
-const NotificationCourse = (props: any) => {
-  const columns = [
-    {
-      title: "Thông báo",
-      dataIndex: "NotificationTitle",
-      render: (text) => <p className="font-weight-blue">{text}</p>,
-    },
-    {
-      title: "Nội dung",
-      dataIndex: "NotificationContent",
-    },
-    {
-      title: "Người tạo",
-      dataIndex: "CreatedBy",
-    },
-    {
-      title: "Ngày tạo",
-      dataIndex: "CreatedOn",
-      render: (CreatedOn) => moment(CreatedOn).format("DD/MM/YYYY"),
-    },
-  ];
-
-  const { showNoti } = useWrap();
-  const [notificationCourse, setNotificationCourse] =
-    useState<INotificationCourse>();
-
-  const [isLoading, setIsLoading] = useState({
-    type: "GET_ALL",
-    status: false,
-  });
-
-  const [currentPage, setCurrentPage] = useState(1);
-  const [totalPage, setTotalPage] = useState(null);
-
-  const listParamsDefault = {
-    pageSize: 10,
-    pageIndex: currentPage,
-    CourseID: props.courseID,
-  };
-
-  const [params, setParams] = useState(listParamsDefault);
-
-  const getPagination = (pageNumber: number) => {
-    setCurrentPage(pageNumber);
-    setParams({
-      ...params,
-      pageIndex: currentPage,
-    });
-  };
-
-  const getDataNotificationCourse = (page: any) => {
-    setIsLoading({
-      type: "GET_ALL",
-      status: true,
-    });
-    (async () => {
-      try {
-        let res = await notificationCourseApi.getAll({
-          ...params,
-          pageIndex: page,
-        });
-        //@ts-ignore
-        res.status == 200 && setNotificationCourse(res.data.data);
-        if (res.status == 204) {
-          showNoti("danger", "Không tìm thấy dữ liệu!");
-          setCurrentPage(1);
-          setParams(listParamsDefault);
-        } else setTotalPage(res.data.totalRow);
-      } catch (error) {
-        showNoti("danger", error.message);
-      } finally {
-        setIsLoading({
-          type: "GET_ALL",
-          status: false,
-        });
-      }
-    })();
-  };
-
-  useEffect(() => {
-    getDataNotificationCourse(currentPage);
-  }, [params]);
-
-  return (
-    <PowerTable
-      currentPage={currentPage}
-      loading={isLoading}
-      totalPage={totalPage && totalPage}
-      getPagination={(pageNumber: number) => getPagination(pageNumber)}
-      addClass="basic-header"
-      TitlePage="Thông báo khóa học"
-      TitleCard={
-        <NotificationCreate
-          reloadData={(firstPage) => {
-            setCurrentPage(1);
-            getDataNotificationCourse(firstPage);
-          }}
-        />
-      }
-      dataSource={notificationCourse}
-      columns={columns}
-      Extra="Thông báo khóa học"
-    />
-  );
+NotificationCourse.propTypes = {
+	courseID: PropTypes.number,
 };
-NotificationCourse.layout = LayoutBase;
+NotificationCourse.defaultProps = {
+	courseID: 0,
+};
+function NotificationCourse(props) {
+	const {courseID} = props;
+	const {showNoti} = useWrap();
+	const [notificationList, setNotificationList] =
+		useState<INotificationCourse[]>();
+	const [isLoading, setIsLoading] = useState({
+		type: 'GET_ALL',
+		status: false,
+	});
+	const [totalPage, setTotalPage] = useState(null);
+	const listFieldInit = {
+		pageSize: 10,
+		pageIndex: 1,
+		CourseID: courseID,
+	};
+	const [filters, setFilters] = useState(listFieldInit);
+	// PAGINATION
+	const getPagination = (pageIndex: number) => {
+		setFilters({
+			...filters,
+			pageIndex,
+		});
+	};
+
+	const getDataNotificationCourse = async () => {
+		try {
+			setIsLoading({
+				type: 'GET_ALL',
+				status: true,
+			});
+			const res = await notificationCourseApi.getAll(filters);
+			if (res.status === 200) {
+				setNotificationList(res.data.data);
+				setTotalPage(res.data['TotalRow']);
+			}
+		} catch (error) {
+			showNoti('danger', error.message);
+		} finally {
+			setIsLoading({
+				type: 'GET_ALL',
+				status: false,
+			});
+		}
+	};
+
+	useEffect(() => {
+		getDataNotificationCourse();
+	}, [filters]);
+
+	const onSubmit = async (data: {
+		NotificationTitle: string;
+		NotificationContent: string;
+	}) => {
+		try {
+			setIsLoading({
+				type: 'ADD_NOTI',
+				status: true,
+			});
+			const newData = {
+				...data,
+				CourseID: courseID,
+			};
+			const res = await notificationCourseApi.add(newData);
+			if (res.status === 200) {
+				showNoti('success', res.data.message);
+				setFilters({
+					...listFieldInit,
+				});
+				return true;
+			}
+		} catch (error) {
+			showNoti('danger', error.message);
+		} finally {
+			setIsLoading({
+				type: 'ADD_NOTI',
+				status: false,
+			});
+		}
+	};
+
+	const columns = [
+		{
+			title: 'Thông báo',
+			dataIndex: 'NotificationTitle',
+			render: (text) => <p className="font-weight-blue">{text}</p>,
+		},
+		{
+			title: 'Nội dung',
+			dataIndex: 'NotificationContent',
+		},
+		{
+			title: 'Người tạo',
+			dataIndex: 'CreatedBy',
+		},
+		{
+			title: 'Ngày tạo',
+			dataIndex: 'CreatedOn',
+			render: (CreatedOn) => moment(CreatedOn).format('DD/MM/YYYY'),
+		},
+	];
+
+	return (
+		<PowerTable
+			currentPage={filters.pageIndex}
+			totalPage={totalPage}
+			getPagination={getPagination}
+			loading={isLoading}
+			addClass="basic-header"
+			TitleCard={
+				<NotificationCourseForm isLoading={isLoading} handleSubmit={onSubmit} />
+			}
+			dataSource={notificationList}
+			columns={columns}
+			Extra="Thông báo khóa học"
+		/>
+	);
+}
 export default NotificationCourse;
