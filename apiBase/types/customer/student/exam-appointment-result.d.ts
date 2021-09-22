d7bc0b99a70e7dda28a8d221095972ea3428cb05
BaoFE98 <kimbao756@yahoo.com>
2021-09-22 23:13:43 +0700

    UPDATE: InfoTestCard

--- apiBase/types/customer/student/exam-appointment-result.d.ts
+++ apiBase/types/customer/student/exam-appointment-result.d.ts
@@ -1,19 +1,15 @@
 type IExamAppointmentResult = IBaseApi<{
-    ExamAppointmentID: number,
-    UserInformationID: number,
-    ListeningPoint: number,
-    SpeakingPoint: number,
-    ReadingPoint: number,
-    WritingPoint: number,
-    VocabPoint: number,
-    MaxTuitionOfStudent: number,
-    TeacherID: number,
-    TeacherName: string,
-    CounselorsID: number,
-    CounselorsName: string,
-    Enable: boolean,
-    CreatedOn: string,
-    CreatedBy: string,
-    ModifiedOn: string,
-    ModifiedBy: string
-}>
+	ExamAppointmentID: number;
+	UserInformationID: number;
+	ListeningPoint: number;
+	SpeakingPoint: number;
+	ReadingPoint: number;
+	WritingPoint: number;
+	VocabPoint: number;
+	MaxTuitionOfStudent: number;
+	TeacherID: number;
+	TeacherName: string;
+	CounselorsID: number;
+	CounselorsName: string;
+	Note: string;
+}>;
