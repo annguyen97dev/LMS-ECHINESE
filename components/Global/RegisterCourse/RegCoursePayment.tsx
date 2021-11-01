6cbf7fda85e81c4b3f7b387e08be6b5bc9349d65
annguyen97dev <annguyen97dev@gmail.com>
2021-11-01 17:42:38 +0700

    update 5

--- components/Global/RegisterCourse/RegCoursePayment.tsx
+++ components/Global/RegisterCourse/RegCoursePayment.tsx
@@ -1,42 +1,42 @@
 import moment from 'moment';
 import Link from 'next/link';
-import React, {useEffect, useState} from 'react';
-import {branchApi} from '~/apiBase';
-import {courseStudentPriceApi} from '~/apiBase/customer/student/course-student-price';
+import React, { useEffect, useState } from 'react';
+import { branchApi } from '~/apiBase';
+import { courseStudentPriceApi } from '~/apiBase/customer/student/course-student-price';
 import PowerTable from '~/components/PowerTable';
-import {useWrap} from '~/context/wrap';
-import {fmSelectArr} from '~/utils/functions';
+import { useWrap } from '~/context/wrap';
+import { fmSelectArr } from '~/utils/functions';
 import CourseOfStudentPriceForm from '../Customer/Finance/CourseOfStudentPrice/CourseStudentPriceForm';
 
 const RegCoursePayment = (props: any) => {
 	const [isLoading, setIsLoading] = useState({
 		type: 'GET_ALL',
-		status: false,
+		status: false
 	});
-	const {userID} = props;
+	const { userID } = props;
 	const [paymentInfo, setPaymentInfo] = useState([]);
-	const {showNoti} = useWrap();
+	const { showNoti } = useWrap();
 	const [optionBranchList, setOptionBranchList] = useState<IOptionCommon[]>([]);
 	const paymentMethodOptionList = [
 		{
 			label: 'Tiền mặt',
-			value: 1,
+			value: 1
 		},
 		{
 			label: 'Chuyển khoản',
-			value: 2,
-		},
+			value: 2
+		}
 	];
 
 	const getDataJob = () => {
 		setIsLoading({
 			type: 'GET_ALL',
-			status: true,
+			status: true
 		});
 		(async () => {
 			try {
 				let res = await courseStudentPriceApi.getAll({
-					UserInformationID: userID,
+					UserInformationID: userID
 				});
 				res.status == 200 && setPaymentInfo(res.data.data);
 				if (res.status == 204) {
@@ -48,7 +48,7 @@ const RegCoursePayment = (props: any) => {
 			} finally {
 				setIsLoading({
 					type: 'GET_ALL',
-					status: false,
+					status: false
 				});
 			}
 		})();
@@ -56,7 +56,7 @@ const RegCoursePayment = (props: any) => {
 
 	const fetchBranchList = async () => {
 		try {
-			const res = await branchApi.getAll({pageSize: 99999, pageIndex: 1});
+			const res = await branchApi.getAll({ pageSize: 99999, pageIndex: 1 });
 			if (res.status === 200) {
 				const fmOptionBranch = fmSelectArr(res.data.data, 'BranchName', 'ID');
 				setOptionBranchList(fmOptionBranch);
@@ -81,17 +81,16 @@ const RegCoursePayment = (props: any) => {
 			try {
 				setIsLoading({
 					type: 'ADD_DATA',
-					status: true,
+					status: true
 				});
-				const {FullNameUnicode, PaymentMethodsID, PayBranchID, Paid, PayDate} =
-					data;
+				const { FullNameUnicode, PaymentMethodsID, PayBranchID, Paid, PayDate } = data;
 				const newData = {
 					ID,
 					FullNameUnicode,
 					PaymentMethodsID,
 					PayBranchID,
 					Paid: parseInt(Paid.replace(/\D/g, '')),
-					PayDate: moment(PayDate).format('YYYY/MM/DD'),
+					PayDate: moment(PayDate).format('YYYY/MM/DD')
 				};
 				const res = await courseStudentPriceApi.update(newData);
 				if (res.status === 200) {
@@ -104,7 +103,7 @@ const RegCoursePayment = (props: any) => {
 			} finally {
 				setIsLoading({
 					type: 'ADD_DATA',
-					status: false,
+					status: false
 				});
 			}
 		};
@@ -120,72 +119,53 @@ const RegCoursePayment = (props: any) => {
 							key={item.ID}
 							href={{
 								pathname: '/course/course-list/course-list-detail/[slug]',
-								query: {slug: item.ID},
+								query: { slug: item.ID }
 							}}
 						>
-							<a
-								title={item.CourseName}
-								className="finance-course-name font-weight-black d-block"
-							>
+							<a title={item.CourseName} className="finance-course-name font-weight-black d-block">
 								{item.CourseName}
 							</a>
 						</Link>
 					))}
 				</>
-			),
+			)
 		},
 		{
 			title: 'Trung tâm',
 			dataIndex: 'PayBranchName',
-			render: (text) => <p className="font-weight-black">{text}</p>,
+			render: (text) => <p className="font-weight-black">{text}</p>
 		},
 		{
 			title: 'Tổng thanh toán',
 			dataIndex: 'Price',
-			render: (price) => (
-				<p className="font-weight-blue">
-					{Intl.NumberFormat('en-US').format(price)}
-				</p>
-			),
+			render: (price) => <p className="font-weight-blue">{Intl.NumberFormat('en-US').format(price)}</p>
 		},
 		{
 			title: 'Giảm giá',
 			dataIndex: 'Reduced',
-			render: (price) => (
-				<p className="font-weight-blue">
-					{Intl.NumberFormat('en-US').format(price)}
-				</p>
-			),
+			render: (price) => <p className="font-weight-blue">{Intl.NumberFormat('en-US').format(price)}</p>
 		},
 		{
 			title: 'Đã thanh toán',
 			dataIndex: 'Paid',
-			render: (price) => (
-				<p className="font-weight-blue">
-					{Intl.NumberFormat('en-US').format(price)}
-				</p>
-			),
+			render: (price) => <p className="font-weight-blue">{Intl.NumberFormat('en-US').format(price)}</p>
 		},
 		{
 			title: 'Số tiền còn lại',
 			dataIndex: 'MoneyInDebt',
-			render: (price) => (
-				<p className="font-weight-blue">
-					{Intl.NumberFormat('en-US').format(price)}
-				</p>
-			),
+			render: (price) => <p className="font-weight-blue">{Intl.NumberFormat('en-US').format(price)}</p>
 		},
 		{
 			title: 'Hình thức',
-			dataIndex: 'PaymentMethodsName',
+			dataIndex: 'PaymentMethodsName'
 		},
 		{
 			title: 'Ngày hẹn trả',
 			dataIndex: 'PayDate',
-			render: (date) => (date ? moment(date).format('DD/MM/YYYY') : ''),
+			render: (date) => (date ? moment(date).format('DD/MM/YYYY') : '')
 		},
 		{
-			render: (data, record: ICourseOfStudentPrice) => (
+			render: (data, record: any) => (
 				<div onClick={(e) => e.stopPropagation()}>
 					{!record.DonePaid && (
 						<CourseOfStudentPriceForm
@@ -199,21 +179,14 @@ const RegCoursePayment = (props: any) => {
 						/>
 					)}
 				</div>
-			),
-		},
+			)
+		}
 	];
 
 	useEffect(() => {
 		getDataJob();
 	}, [userID]);
 
-	return (
-		<PowerTable
-			Extra="Thông tin thanh toán"
-			columns={columns}
-			noScroll
-			dataSource={paymentInfo}
-		/>
-	);
+	return <PowerTable Extra="Thông tin thanh toán" columns={columns} noScroll dataSource={paymentInfo} />;
 };
 export default RegCoursePayment;
