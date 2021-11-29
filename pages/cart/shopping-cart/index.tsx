419d6dd91b876ea2758628db7084df2b8971ab47
tamchamhai <ngophilongspkt@gmail.com>
2021-11-29 17:02:43 +0700

    update

--- pages/cart/shopping-cart/index.tsx
+++ pages/cart/shopping-cart/index.tsx
@@ -116,14 +116,19 @@ const ShoppingCart = () => {
 	const renderCartItems = () => {
 		return cartItems?.map((item, index) => (
 			<div className=" cart__item d-flex justify-content-between align-items-center row" key={index}>
-				<div className="cart__item-img col-3">
+				<div className="cart__item-img col-6 col-sm-3">
 					<Link href="/">
 						<a href="#">
 							<img src={item.ImageThumbnails.length ? item.ImageThumbnails : '/images/cat-ben.jpg'} alt="img course"></img>
 						</a>
 					</Link>
 				</div>
-				<div className="cart__item-detail col-4">
+				<div className="cart__item-action d-inline-block d-sm-none col-6 col-sm-3">
+					<p>Xóa</p>
+					<p>Lưu xem sau</p>
+					<p>Thêm vào yêu thích</p>
+				</div>
+				<div className="cart__item-detail col-12 col-sm-4 mt-3 mb-3 mt-sm-0 mb-sm-0">
 					<h5>{item.VideoCourseName}</h5>
 					{/* <p>Tạo bởi: Dr. Angele Yu</p> */}
 					{/* <div className="detail__hours">
@@ -132,12 +137,12 @@ const ShoppingCart = () => {
 						<span>Trung cấp</span>
 					</div> */}
 				</div>
-				<div className="cart__item-action col-3">
+				<div className="cart__item-action d-none d-sm-inline-block col-12 col-sm-3">
 					<p>Xóa</p>
 					<p>Lưu xem sau</p>
 					<p>Thêm vào yêu thích</p>
 				</div>
-				<div className="cart__item-price col-2">
+				<div className="cart__item-price col-12 col-sm-2">
 					<p className="font-weight-green">{numberWithCommas(item.Price)} vnd</p>
 				</div>
 			</div>
@@ -203,7 +208,7 @@ const ShoppingCart = () => {
 	}, []);
 
 	return (
-		<div style={{ backgroundColor: '#fff' }}>
+		<div>
 			<header>
 				<div className="shopping__cart-header justify-content-between align-items-center row">
 					<div className="header__logo col-6 col-md-3">
@@ -273,9 +278,9 @@ const ShoppingCart = () => {
 				{isLoading.loading ? (
 					<Skeleton active />
 				) : (
-					<div className="shopping__cart-content row mt-3">
-						<div className="shopping__cart-items col-12 col-md-8">{renderCartItems()}</div>
-						<div className="shopping__cart-total col-12 col-md-4 mt-5 mt-md-0">
+					<div className="shopping__cart-content row mt-3 mb-3">
+						<div className="shopping__cart-items col-12 col-lg-8">{renderCartItems()}</div>
+						<div className="shopping__cart-total col-12 col-lg-4 mt-5 mt-md-0 mb-3">
 							<h4>Tổng cộng: </h4>
 							<h1 className="font-weight-green">
 								{numberWithCommas(cartItems?.reduce((a, b) => Number(a) + Number(b.Price), 0))}
