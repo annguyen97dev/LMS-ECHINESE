1ce46032ef6ea200c3a6c6f3e083c6e13c794759
chaudev <soc900k@gmail.com>
2021-12-10 15:24:02 +0700

    x

--- appConfig.js
+++ appConfig.js
@@ -1,5 +1,5 @@
 const _ = {
-	API_URL: 'https://lmsv2.monamedia.net' // TEST LINK
-	// API_URL: 'http://api.echinese.vn/' // LIVE LINK
+	// API_URL: 'https://lmsv2.monamedia.net' // TEST LINK
+	API_URL: 'http://api.echinese.vn/' // LIVE LINK
 };
 export default _;
