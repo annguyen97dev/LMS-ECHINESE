a344188e5a87a13e3cf80e6cea240cc174db5f09
tamchamhai <ngophilongspkt@gmail.com>
2021-12-18 21:54:48 +0700

    update trial and limit student

---
+++ components/Global/RegisterCourse/OverStudentConfirmBox.tsx
@@ -0,0 +1,55 @@
+import { Modal, Spin } from 'antd';
+import React, { useEffect, useState } from 'react';
+
+const OverStudentConfirmBox = (props) => {
+	const {
+		visibleModalConfirm,
+		setVisibleModalConfirm,
+		courseOverStudent,
+		setCourseOverStudent,
+		courseOverStudentClone,
+		onSubmit,
+		dataSubmit,
+		isLoading
+	} = props;
+	courseOverStudent && console.log(courseOverStudent[0].courseName);
+	return (
+		<>
+			<Modal
+				visible={visibleModalConfirm}
+				title="Xác nhận"
+				footer={null}
+				onCancel={() => {
+					setVisibleModalConfirm(false);
+					setCourseOverStudent(courseOverStudentClone);
+				}}
+			>
+				<div className="row">
+					<div className="col-12 mb-3">
+						{courseOverStudentClone && (
+							<p className="font-weigth-primary">
+								Khóa học {courseOverStudentClone.map((item) => item.courseName).join(' và ')} đã đầy
+							</p>
+						)}
+					</div>
+					<div className="col-12 mb-3">
+						<h5 className="text-center">Xác nhận vẫn thêm học viên vào lớp</h5>
+					</div>
+					<div className="col-12">
+						<button
+							type="submit"
+							className="btn btn-primary w-100"
+							onClick={() => {
+								onSubmit(dataSubmit);
+							}}
+							disabled={isLoading}
+						>
+							{isLoading ? <Spin /> : 'Xác nhận'}
+						</button>
+					</div>
+				</div>
+			</Modal>
+		</>
+	);
+};
+export default OverStudentConfirmBox;
