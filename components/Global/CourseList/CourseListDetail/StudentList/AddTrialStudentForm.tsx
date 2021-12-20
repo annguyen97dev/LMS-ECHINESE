defec859ffb31e103d2c5042a3a271eb420d0304
chaudev <soc900k@gmail.com>
2021-12-20 17:18:24 +0700

    Update add student to trial

--- components/Global/CourseList/CourseListDetail/StudentList/AddTrialStudentForm.tsx
+++ components/Global/CourseList/CourseListDetail/StudentList/AddTrialStudentForm.tsx
@@ -20,9 +20,26 @@ const AddTrialStudentForm = (props) => {
 		pageIndex: 1,
 		CourseID: CourseID
 	});
+	const [userDetail, setUserDetail] = useState<IStudent>();
+	const [isLoadingX, setIsLoadingx] = useState(false);
 
 	const { Option } = Select;
 	const { TextArea } = Input;
+
+	const handleChangeUser = (value) => {
+		setIsLoadingx(true);
+		(async () => {
+			try {
+				const _detail = await studentApi.getWithID(value);
+				//@ts-ignore
+				_detail.status == 200 && setUserDetail(_detail.data.data);
+			} catch (err) {
+				showNoti('danger', err.message);
+			} finally {
+				setIsLoadingx(false);
+			}
+		})();
+	};
 
 	const [params, setParams] = useState({
 		CourseID: CourseID,
@@ -115,78 +132,225 @@ const AddTrialStudentForm = (props) => {
 				}}
 				title="Thêm học viên học thử"
 				visible={visible}
+				width={700}
 			>
 				<Form form={form} onFinish={_onSubmit} layout="vertical">
 					<div className="row">
-						<div className="col-12 mb-3">
-							<Form.Item
-								label="Email học viên"
-								name="UserInformationID"
-								rules={[{ required: true, message: 'Bạn không được để trống' }]}
-							>
-								<Select
-									placeholder="Chọn học viên"
-									className="style-input"
-									style={{ width: '100%' }}
-									onChange={handleChangeSelect}
-									size="large"
-									showSearch
-									filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+						<div className="row m-0" style={{ width: '100%' }}>
+							<div className="col-md-6 col-12">
+								<Form.Item
+									name="UserInformationID"
+									label="Email"
+									rules={[
+										{
+											required: true,
+											message: 'Vui lòng điền đủ thông tin!'
+										}
+									]}
 								>
-									{students &&
-										students.map((item, index) => (
-											<Option key={index} value={item.UserInformationID}>
-												{item.Email}
-											</Option>
-										))}
-								</Select>
-							</Form.Item>
+									<Select
+										placeholder="Chọn học viên"
+										className="style-input"
+										style={{ width: '100%' }}
+										onChange={(value) => handleChangeUser(value)}
+										size="large"
+										showSearch
+										filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+									>
+										{students &&
+											students.map((item, index) => (
+												<Option key={index} value={item.UserInformationID}>
+													{item.Email}
+												</Option>
+											))}
+									</Select>
+								</Form.Item>
+							</div>
+
+							<div className="col-md-6 col-12">
+								<Form.Item label="Họ và tên">
+									<Input value={userDetail ? userDetail.FullNameUnicode : ''} className="style-input" readOnly={true} />
+								</Form.Item>
+							</div>
 						</div>
-						<div className="col-12 mb-3">
-							<Form.Item
-								label="Email học viên"
-								name="TrialStart"
-								rules={[{ required: true, message: 'Bạn không được để trống' }]}
-							>
-								<Select
-									placeholder="Chọn ngày bắt đầu"
-									className="style-input"
-									style={{ width: '100%' }}
-									onChange={onSelectCourseScheduleStart}
+						{/*  */}
+						{/*  */}
+						<div className="row m-0" style={{ width: '100%' }}>
+							<div className="col-md-6 col-12">
+								<Form.Item label="Ngày sinh">
+									<Input
+										readOnly={true}
+										className="style-input"
+										value={userDetail ? moment(userDetail.DOB).format('DD/MM/YYYY') : ''}
+									/>
+								</Form.Item>
+							</div>
+							<div className="col-md-6 col-12">
+								<Form.Item label="SĐT">
+									<Input readOnly={true} value={userDetail ? userDetail.Mobile : ''} className="style-input" />
+								</Form.Item>
+							</div>
+						</div>
+						{/*  */}
+						{/*  */}
+						<div className="row m-0" style={{ width: '100%' }}>
+							<div className="col-md-6 col-12">
+								<Form.Item label="Tỉnh/Thành phố">
+									<Input readOnly={true} className="style-input" value={userDetail ? userDetail.AreaName : ''} />
+								</Form.Item>
+							</div>
+							<div className="col-md-6 col-12">
+								<Form.Item label="Quận/Huyện">
+									<Input readOnly={true} value={userDetail ? userDetail.DistrictName : ''} className="style-input" />
+								</Form.Item>
+							</div>
+						</div>
+						{/*  */}
+						{/*  */}
+						<div className="row m-0" style={{ width: '100%' }}>
+							<div className="col-md-6 col-12">
+								<Form.Item label="Phường xã">
+									<Input readOnly={true} value={userDetail ? userDetail.WardName : ''} className="style-input" />
+								</Form.Item>
+							</div>
+							<div className="col-md-6 col-12">
+								<Form.Item label="Địa chỉ - Mô tả">
+									<Input
+										readOnly={true}
+										value={
+											userDetail
+												? `${userDetail.HouseNumber ? userDetail.HouseNumber : ''} ${
+														userDetail.Address ? userDetail.Address : ''
+												  }`
+												: ''
+										}
+										className="style-input"
+									/>
+								</Form.Item>
+							</div>
+						</div>
+						{/*  */}
+						{/*  */}
+						<div className="row m-0" style={{ width: '100%' }}>
+							<div className="col-md-6 col-12">
+								<Form.Item label="CMND">
+									<Input readOnly={true} value={userDetail ? userDetail.CMND : ''} className="style-input" />
+								</Form.Item>
+							</div>
+							<div className="col-md-6 col-12">
+								<Form.Item label="Nơi cấp">
+									<Input readOnly={true} value={userDetail ? userDetail.CMNDRegister : ''} className="style-input" />
+								</Form.Item>
+							</div>
+						</div>
+						{/*  */}
+						{/*  */}
+						<div className="row m-0" style={{ width: '100%' }}>
+							<div className="col-md-6 col-12">
+								<Form.Item label="Ngày cấp">
+									<Input
+										readOnly={true}
+										value={userDetail?.CMNDDate ? moment(userDetail.CMNDDate).format('DD/MM/YYYY') : ''}
+										className="style-input"
+									/>
+								</Form.Item>
+							</div>
+							<div className="col-md-6 col-12">
+								<Form.Item label="Công việc">
+									<Input readOnly={true} className="style-input" value={userDetail ? userDetail.JobName : ''} />
+								</Form.Item>
+							</div>
+						</div>
+						{/*  */}
+						{/*  */}
+						<div className="row m-0" style={{ width: '100%' }}>
+							<div className="col-md-6 col-12">
+								<Form.Item label="Người nhà, liên hệ">
+									<Input className="style-input" readOnly={true} value={userDetail ? userDetail.ParentsNameOf : ''} />
+								</Form.Item>
+							</div>
+							<div className="col-md-6 col-12">
+								<Form.Item label="Tư vấn viên">
+									<Input
+										className="style-input"
+										readOnly={true}
+										//@ts-ignore
+										value={userDetail ? userDetail.CounselorsName : ''}
+									/>
+								</Form.Item>
+							</div>
+						</div>
+						{/*  */}
+						{/*  */}
+						<div className="row m-0" style={{ width: '100%' }}>
+							<div className="col-md-6 col-12">
+								<Form.Item label="Nguồn khách">
+									<Input
+										readOnly={true}
+										className="style-input"
+										value={userDetail ? userDetail.SourceInformationName : ''}
+									/>
+								</Form.Item>
+							</div>
+							<div className="col-md-6 col-12">
+								<Form.Item label="Mục đích học">
+									<Input
+										readOnly={true}
+										className="style-input"
+										value={userDetail ? userDetail.AcademicPurposesName : ''}
+									/>
+								</Form.Item>
+							</div>
+						</div>
+
+						<div className="row m-0" style={{ width: '100%' }}>
+							<div className="col-md-6 col-12">
+								<Form.Item
+									label="Chọn ngày bắt đầu"
+									name="TrialStart"
+									rules={[{ required: true, message: 'Bạn không được để trống' }]}
 								>
-									{scheduleList &&
-										scheduleList.map((o, idx) => (
-											<Option key={idx} value={o.value}>
-												{o.title}
-											</Option>
-										))}
-								</Select>
-							</Form.Item>
-						</div>
-						<div className="col-12 mb-3">
-							<Form.Item
-								label="Email học viên"
-								name="TrialEnd"
-								rules={[{ required: true, message: 'Bạn không được để trống' }]}
-							>
-								<Select
-									placeholder="Chọn ngày kết thúc"
-									className="style-input"
-									style={{ width: '100%' }}
-									onChange={onSelectCourseScheduleEnd}
+									<Select
+										placeholder="Chọn ngày bắt đầu"
+										className="style-input"
+										style={{ width: '100%' }}
+										onChange={onSelectCourseScheduleStart}
+									>
+										{scheduleList &&
+											scheduleList.map((o, idx) => (
+												<Option key={idx} value={o.value}>
+													{o.title}
+												</Option>
+											))}
+									</Select>
+								</Form.Item>
+							</div>
+							<div className="col-md-6 col-12">
+								<Form.Item
+									label="Chọn ngày kết thúc"
+									name="TrialEnd"
+									rules={[{ required: true, message: 'Bạn không được để trống' }]}
 								>
-									{scheduleList &&
-										scheduleList.map((o, idx) => (
-											<Option key={idx} value={o.value}>
-												{o.title}
-											</Option>
-										))}
-								</Select>
-							</Form.Item>
+									<Select
+										placeholder="Chọn ngày kết thúc"
+										className="style-input"
+										style={{ width: '100%' }}
+										onChange={onSelectCourseScheduleEnd}
+									>
+										{scheduleList &&
+											scheduleList.map((o, idx) => (
+												<Option key={idx} value={o.value}>
+													{o.title}
+												</Option>
+											))}
+									</Select>
+								</Form.Item>
+							</div>
 						</div>
+
 						<div className="col-12 mb-3">
 							<Form.Item label="Ghi chú" name="Note">
-								<TextArea rows={4} className="style-input" placeholder="Thêm ghi chú" />
+								<TextArea rows={5} className="style-input" placeholder="Thêm ghi chú" />
 							</Form.Item>
 						</div>
 						<div className="col-12">
